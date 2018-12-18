b3d7548604247ca7cc4bc9a0105f818f8db923c8
fpeiro <43823450+fpeiro@users.noreply.github.com>
2018-12-18 03:46:34 +0100

    Actualizada automatización

--- automatizacion/acopio.sh
+++ automatizacion/acopio.sh
@@ -6,15 +6,15 @@ NSG_NAME=MV-CC2NSG
 MV_NAME=MV-CC2
 
 # Se crea un grupo de recursos llamado Recursos-MV-CC2
-az group create --name $RES_GROUP --location eastus
+az group create --name $RES_GROUP --location uksouth
 
 # Se crea una máquina virtual utilizando el grupo de recursos
 az vm create --resource-group $RES_GROUP \
   --name $MV_NAME \
   --nsg $NSG_NAME \
   --image UbuntuLTS \
-  --size Standard_B1s \ # Con el tamaño más básico
-  --generate-ssh-keys \ # Generando las claves para realizar la conexión
+  --size Standard_B1s `# Con el tamaño más básico` \
+  --generate-ssh-keys `# Generando las claves para realizar la conexión` \
   --public-ip-address-allocation static \
   --output json \
   --verbose > .output
